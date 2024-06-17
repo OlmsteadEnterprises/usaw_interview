51c4f62aed3ed4a7441747f328a85821bf2b89b0
OlmsteadEnterprises <travis.olmstead96@gmail.com>
2024-06-16 18:24:04 -0600

    Core task completed

--- usaw-app/resources/js/Pages/USAW/Usaw.vue
+++ usaw-app/resources/js/Pages/USAW/Usaw.vue
@@ -1,5 +1,6 @@
 <template>
     <AppLayout>
+        <div class="flex justify-center text-[25px] font-bold">{{$page.props.appName}}</div>
         <USAWForm />
         <div class="mt-3">
             <USAWTable />
@@ -13,7 +14,7 @@ import USAWForm from "@/Components/USAW/USAWForm.vue";
 import USAWTable from "@/Components/USAW/USAWTable.vue";
 export default {
     name: "Usaw",
-    components: {USAWTable, USAWForm, AppLayout}
+    components: {USAWTable, USAWForm, AppLayout},
 }
 </script>
 
